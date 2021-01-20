e26b7ef4919f2966995d9ee994295be589c8bb5c
Jason Herrera <baytownjason@yahoo.com>
2021-01-20 13:03:05 -0600

    2 failures

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,18 @@
+class Appointment
+    
+    attr_reader :doctor, :patient, :date
+    
+    @@all = []
 
+    def initialize(date, patient, doctor)
+        @doctor = doctor
+        @patient = patient
+        @date = date
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,25 @@
+class Doctor
+  
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+    end
+    def appointments
+        Appointment.all.select {|appointment| appointment.doctor == self}
+    end
+    def patients
+        Appointment.all.find_all {|appointment| appointment.doctor == self}.map {|appointment| appointment.patient & appointment.date}
+    end
+end
